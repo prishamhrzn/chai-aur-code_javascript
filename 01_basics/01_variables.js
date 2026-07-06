63c0c5f437c95979b6b0403e3eab2760a39af843
prishamhrzn <csit80117722_prisha@achsnepal.edu.np>
2026-07-06 16:34:51 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const pie = 3.1415;
+const accountId = 678;
+
+let name ="Prisha"
+ console.log(accountId);
---
+++ 01_basics/test.js
@@ -0,0 +1,8 @@
+
+
+let age = 26
+console.log("Prisha")
+console.log("age")
+console.log(typeof "Prisha")
+console.log(typeof age);
+console.log(typeof 29)

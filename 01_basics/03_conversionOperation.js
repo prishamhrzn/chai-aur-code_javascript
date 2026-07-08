6a78976acb781d6d98a56bf77027c3c3c67c6faf
prishamhrzn <csit80117722_prisha@achsnepal.edu.np>
2026-07-08 16:27:10 +0000

    Datatype Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// Datatype Conversion
+
+let score = 89; //number datatype
+
+console.log(typeof score);  
+ console.log(typeof (score));  //yo 2 ta ma jun pani use garna milxa
+
+ let stringvalue = String(score);  //89 van ne number lai string ma lageko
+ console.log(stringvalue);
+ console.log(typeof stringvalue);  
+
+let marks = "56";  //string datatype
+
+console.log(typeof marks);
+
+let valueInNumber = Number(marks); //String lai number datatype ma convert gareko
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+let isLoggedIn = true;
+console.log(isloggedIn);
+console.log(typeof isLoggedIn);

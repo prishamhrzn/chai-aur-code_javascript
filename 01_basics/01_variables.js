3cbb5474173aa2cb1b846ae3c5ab171e2177ee37
prishamhrzn <csit80117722_prisha@achsnepal.edu.np>
2026-07-21 12:48:22 +0000

    String

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,5 +1,20 @@
 const pie = 3.1415;
 const accountId = 678;
 
-let name ="Prisha"
+let name ="Prisha";
+let accountEmail = "prisha@gmail.com";
+var accountPassword ="1234";  
+accountCity = "Lubhu"; // don't declare variables like this
+let accountState;
+
+accountEmail = "pm35@gmail.com";
+accountPassword = "869";
+accountCity = "Imadol"
+
  console.log(accountId);
+ console.table([name, accountEmail, accountPassword, accountCity, accountState]);
+
+ /*
+ Prefer not to use var bacause of issue in block scope and functuonal scope
+ */
+ 
--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -23,12 +23,34 @@ console.log(gameName)
 console.log(gameName.indexOf("p"))
 
 //New Method
-const gameOne = new String("prisha-coc");
+const gameOne = new String("prishamhz");
 const newString = gameOne.substring(0,4) //pris sama auxa 0 to 3 sama, 4 chai audaina //Substring ma -ve value rakhna mildaina
 console.log(newString);
 
-const anotherString = gameOne.slice(-8,4) //slice ma negative value rakhna milxa ani it prints from last
+const anotherString = gameOne.slice(-8,4) //slice ma negative value rakhna milxa ani it prints from last //last bata herne jastai -1 -2....-8 ani tespaxi tyo string bata arko 3 ta print gareko
 console.log(anotherString);
+
+//trim => starting ra end space lai remove gardinxaa
+const newStringOne = "    prisha      ";
+console.log(newStringOne)
+console.log(newStringOne.trim());
+
+//replace
+const url = "https://prisha.com/prisha%20maharjan";
+console.log(url.replace('%20', '*')); // hamle %20 lai * sanga replace gardeko
+
+console.log(url.includes('prisha'))  //true kina ki prisha is included in my url
+console.log(url.includes('mhz'))   //false hunxa kinaki mhz url mai xna
+
+//split
+const StringTwo = "prisha-maharjan-29-00";
+console.log(StringTwo.split("-")); //StringTwo ko string lai chai hamle xuta xutai lekheko based on - 
+
+const StringThree = "prisha maharjan 13 99";
+console.log(StringThree.split(" "));   //split on the basis of space //yo split le chai array ma haldinxa string lai on the basis of space
+
+
+
 
 
 
